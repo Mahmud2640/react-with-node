ab9075806138ceb2fcdb0e512b4ee87ffeb675a1
Mahmud2640 <mdmahmud484@gmail.com>
2022-04-30 14:27:32 +0600

    dataa show UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+    fetch("http://localhost:5000/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
   return (
     <div className="App">
       <h1>Hello this is Server..</h1>
-      <p>I can build my on database!!!!</p>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="Name" required /> <br />
+        <input type="text" name="email" placeholder="Email" required /> <br />
+        <input type="submit" value="Add user" />
+      </form>
+      <h1>I can run : {users.length}</h1>
+      {users.map((user) => (
+        <h1 key={user.id}>
+          Name: {user.name} <br /> Email: {user.email}
+        </h1>
+      ))}
     </div>
   );
 }
